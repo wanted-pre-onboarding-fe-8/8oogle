6e649ae5d4b5963e4864e8573acb97b97d7d9996
bebusl <bebus1998@naver.com>
2022-07-14 11:53:53 +0900

    chore: Layout outlet 추가

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import HeaderBar from "./HeaderBar";
 import SideBar from "./SideBar";
+
 function DefaultLayout() {
   return (
     <>
       <HeaderBar />
       <SideBar />
+      <Outlet />
     </>
   );
 }
