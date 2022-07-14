9c1b231d9997140d071e653ffdc6fdae028f85a6
ChuYeonBin <cndusqls98@gmail.com>
2022-07-14 18:10:12 +0900

    feat #2: 카드 골격 구현

--- src/pages/adManagement/Card.tsx
+++ src/pages/adManagement/Card.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { ICampaignItem } from '../../types/campaign';
+import {
+  Button,
+  Card as DefaultCard,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+} from '@mui/material';
 
 // interface CardProps {
 //   campaignItem: ICampaignItem;
 // }
 
-function Card(/*{ campaignItem }: CardProps*/) {
+function Card() {
   const campaignItem: ICampaignItem = {
     id: 1,
     adType: 'web',
@@ -20,7 +32,47 @@ function Card(/*{ campaignItem }: CardProps*/) {
       roas: 433,
     },
   };
-  return <></>;
+  return (
+    <DefaultCard variant='outlined'>
+      <CardHeader title={campaignItem.title} />
+      <CardContent>
+        <TableContainer>
+          <Table size='small'>
+            <TableBody>
+              <TableRow style={{ borderTop: '1px solid rgba(224, 224, 224, 1)' }}>
+                <TableCell>상태</TableCell>
+                <TableCell>{campaignItem.status}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>광고 생성일</TableCell>
+                <TableCell>{campaignItem.startDate}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>일 희망 예산</TableCell>
+                <TableCell>{campaignItem.budget}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>광고 수익률</TableCell>
+                <TableCell>{campaignItem.report.roas}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>매출</TableCell>
+                <TableCell>{campaignItem.report.convValue}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>광고 비용</TableCell>
+                <TableCell>{campaignItem.report.cost}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </CardContent>
+      <CardActions>
+        <Button>수정하기</Button>
+        <Button>삭제하기</Button>
+      </CardActions>
+    </DefaultCard>
+  );
 }
 
 export default Card;
