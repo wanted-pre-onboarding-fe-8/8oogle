8a014a67f07ca0716fe52a41581b73d7fdc03d9c
ChuYeonBin <cndusqls98@gmail.com>
2022-07-14 20:01:37 +0900

    feat #2: 카드 컴포넌트 UI 커스터마이징

--- src/pages/adManagement/Card.tsx
+++ src/pages/adManagement/Card.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ICampaignItem } from '../../types/campaign';
+import styled from 'styled-components';
 import {
   Button,
   Card as DefaultCard,
@@ -33,46 +34,65 @@ function Card() {
     },
   };
   return (
-    <DefaultCard variant='outlined'>
-      <CardHeader title={campaignItem.title} />
+    <DefaultCard variant='outlined' style={{ borderRadius: '12px' }}>
+      <CardHeader title={campaignItem.title} sx={{ pb: 0 }} />
       <CardContent>
         <TableContainer>
           <Table size='small'>
             <TableBody>
-              <TableRow style={{ borderTop: '1px solid rgba(224, 224, 224, 1)' }}>
+              <Row style={{ borderTop: '1px solid rgba(224, 224, 224, 1)' }}>
                 <TableCell>상태</TableCell>
                 <TableCell>{campaignItem.status}</TableCell>
-              </TableRow>
-              <TableRow>
+              </Row>
+              <Row>
                 <TableCell>광고 생성일</TableCell>
                 <TableCell>{campaignItem.startDate}</TableCell>
-              </TableRow>
-              <TableRow>
+              </Row>
+              <Row>
                 <TableCell>일 희망 예산</TableCell>
                 <TableCell>{campaignItem.budget}</TableCell>
-              </TableRow>
-              <TableRow>
+              </Row>
+              <Row>
                 <TableCell>광고 수익률</TableCell>
                 <TableCell>{campaignItem.report.roas}</TableCell>
-              </TableRow>
-              <TableRow>
+              </Row>
+              <Row>
                 <TableCell>매출</TableCell>
                 <TableCell>{campaignItem.report.convValue}</TableCell>
-              </TableRow>
-              <TableRow>
+              </Row>
+              <Row>
                 <TableCell>광고 비용</TableCell>
                 <TableCell>{campaignItem.report.cost}</TableCell>
-              </TableRow>
+              </Row>
             </TableBody>
           </Table>
         </TableContainer>
       </CardContent>
-      <CardActions>
-        <Button>수정하기</Button>
-        <Button>삭제하기</Button>
-      </CardActions>
+      <ButtonWrapper sx={{ p: 0, pb: 2 }}>
+        <Button size='small' variant='outlined' color='inherit'>
+          수정하기
+        </Button>
+        <Button size='small' variant='outlined' color='warning'>
+          삭제하기
+        </Button>
+      </ButtonWrapper>
     </DefaultCard>
   );
 }
+
+const Row = styled(TableRow)`
+  & > td:first-child {
+    padding-left: 0;
+    color: gray;
+  }
+`;
+
+const ButtonWrapper = styled(CardActions)`
+  justify-content: space-around;
+  & > button {
+    color: black;
+    border: 1px solid rgba(224, 224, 224, 1);
+  }
+`;
 
 export default Card;
