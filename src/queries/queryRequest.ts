278bb2a20610c2b8078043efa70a493ce565ab93
corgi-world <developer.msw@gmail.com>
2022-07-15 13:58:03 +0900

    refactor: query key startDate로 변경

--- src/queries/queryRequest.ts
+++ src/queries/queryRequest.ts
@@ -5,8 +5,8 @@ import { get } from './httpRequest';
 export function getDashboard(startDate: string, endDate: string) {
   const query = `?date_gte=${startDate}&date_lte=${endDate}`;
 
-  const overall = useQuery([OVERALL, endDate], () => get(overallService, query));
-  const platform = useQuery([PLATFORM, endDate], () => get(platformService, query));
+  const overall = useQuery([OVERALL, startDate], () => get(overallService, query));
+  const platform = useQuery([PLATFORM, startDate], () => get(platformService, query));
 
   return [overall, platform];
 }
