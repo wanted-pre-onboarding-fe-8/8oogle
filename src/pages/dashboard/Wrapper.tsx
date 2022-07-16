0942af6f695b86eeac9345e0303956e73a86e6b4
corgi-world <developer.msw@gmail.com>
2022-07-16 14:59:21 +0900

    chore: 발표 자료 공유

--- src/pages/dashboard/Wrapper.tsx
+++ src/pages/dashboard/Wrapper.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { getDashboard } from '../../queries/queryRequest';
-import AdStatusChart from './adStatusChart';
 import PlatformChart from './PlatformChart';
 
 interface iWrapper {
