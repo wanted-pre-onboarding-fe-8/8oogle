2881eef34a79946e3b064a34d40cf4bd68b43c44
corgi-world <developer.msw@gmail.com>
2022-07-18 13:05:35 +0900

    refactor: 전역에서 사용하는 데이터 관련 상수 분리

---
+++ src/utils/constants/data.ts
@@ -0,0 +1,18 @@
+export const OVERALL_CONSTANTS = {
+  OVERALL: 'overall',
+} as const;
+
+export const PLATFORM_CONSTANTS = {
+  PLATFORM: 'platform',
+  GOOGLE: 'google',
+  KAKAO: 'kakao',
+  NAVER: 'naver',
+  FACEBOOK: 'facebook',
+} as const;
+
+export const CAMPAIGN_CONSTANTS = {
+  CAMPAIGN: 'campaign',
+  STATUS_ALL: 'all',
+  STATUS_ACTIVE: 'active',
+  STATUS_ENDED: 'ended',
+} as const;
