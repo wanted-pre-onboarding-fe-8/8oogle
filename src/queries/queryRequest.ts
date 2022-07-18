601b9ddc1fd7b6ec65147b7835a6917c5b9fc21a
corgi-world <developer.msw@gmail.com>
2022-07-18 13:08:01 +0900

    feat: campaign crud 구현

--- src/queries/queryRequest.ts
+++ src/queries/queryRequest.ts
@@ -1,6 +1,12 @@
-import { useQuery } from 'react-query';
-import { OVERALL, PLATFORM, overallService, platformService } from './services';
-import { get } from './httpRequest';
+import { useQuery, useMutation } from 'react-query';
+import { get, post, put, _delete } from './httpRequest';
+import { typeStatus, ICampaignItemBase } from '../types/campaign';
+import { overallService, platformService, campaignService } from './services';
+
+import { OVERALL_CONSTANTS, PLATFORM_CONSTANTS, CAMPAIGN_CONSTANTS } from '../utils/constants/data';
+const { OVERALL } = OVERALL_CONSTANTS;
+const { PLATFORM } = PLATFORM_CONSTANTS;
+const { CAMPAIGN, STATUS_ALL } = CAMPAIGN_CONSTANTS;
 
 export function getDashboard(startDate: string, endDate: string) {
   const query = `?date_gte=${startDate}&date_lte=${endDate}`;
@@ -9,4 +15,26 @@ export function getDashboard(startDate: string, endDate: string) {
   const platform = useQuery([PLATFORM, startDate], () => get(platformService, query));
 
   return [overall, platform];
+}
+
+export function getCampaignByStatus(status: typeStatus) {
+  const query = status === STATUS_ALL ? '' : `?status=${status}`;
+
+  return useQuery([CAMPAIGN, status], () => get(campaignService, query), { staleTime: 0 });
+}
+
+export function createCampaign(campaign: ICampaignItemBase) {
+  return useMutation(() => post(campaignService, campaign));
+}
+
+export function updateCampaign(id: number, campaign: ICampaignItemBase) {
+  const query = `/${id}`;
+
+  return useMutation(() => put(campaignService, query, campaign));
+}
+
+export function deleteCampaign(id: number) {
+  const query = `/${id}`;
+
+  return useMutation(() => _delete(campaignService, query));
 }
