3284853c49f40bd577d422d3b5a94ecb892d15d6
ohtaekwon <ohtk1119@gmail.com>
2022-07-19 16:49:08 +0900

    feat : data 상수에 overall 요소들 추가

--- src/utils/constants/data.ts
+++ src/utils/constants/data.ts
@@ -1,16 +1,28 @@
 export const OVERALL_CONSTANTS = {
   OVERALL: 'overall',
+<<<<<<< HEAD
   IMP: 'imp',
   CLICK: 'click',
   CONV: 'conv',
   CONV_VALUE: 'convValue',
   COST: 'cost',
+=======
+  ROAS: 'roas',
+  IMP: 'Imp',
+  CLICK: 'click',
+  COST: 'cost',
+  CONV: 'conv',
+  CONVVALUE: 'convValue',
+>>>>>>> 6b9ca34 (feat : data 상수에 overall 요소들 추가)
   CTR: 'ctr',
   CVR: 'cvr',
   CPC: 'cpc',
   CPA: 'cpa',
+<<<<<<< HEAD
   ROAS: 'roas',
   DATE_TYPE_MM_DD: 'MM월 dd일',
+=======
+>>>>>>> 6b9ca34 (feat : data 상수에 overall 요소들 추가)
 } as const;
 
 export const PLATFORM_CONSTANTS = {
