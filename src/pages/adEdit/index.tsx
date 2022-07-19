8630beedcf497a0686e85b3d3b98d03f8bc06f5d
Aum Il-Kyeong <sunaerocket@gmail.com>
2022-07-19 21:10:58 +0900

    refactor: 광고 수정 양식 제목 상수 선언

--- src/pages/adEdit/index.tsx
+++ src/pages/adEdit/index.tsx
@@ -4,6 +4,7 @@ import { ICampaignItem } from '../../types/campaign';
 import AdForm from '../../components/adForm/AdForm';
 
 function AdEdit() {
+  const FORM_TITLE = '광고수정';
   const location = useLocation();
   const navigate = useNavigate();
   const hasNoState = !location.state;
@@ -14,7 +15,7 @@ function AdEdit() {
   }, []);
   const campaign = location.state as ICampaignItem;
 
-  return <AdForm campaignItem={campaign} title='광고수정' />;
+  return <AdForm campaignItem={campaign} title={FORM_TITLE} />;
 }
 
 export default AdEdit;
