88d3149fcc23ac7352f49a9acbc6a495ca2fd958
Aum Il-Kyeong <sunaerocket@gmail.com>
2022-07-19 16:05:40 +0900

    feat: 캠페인 데이터 없이 광고 수정 접근하면 리디렉트 처리

--- src/pages/adEdit/index.tsx
+++ src/pages/adEdit/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 function AdEdit() {
-  return <div>AdEdit</div>;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const hasNoState = !location.state;
+  React.useEffect(() => {
+    if (hasNoState) {
+      navigate('/ad');
+    }
+  }, []);
+  return <Container>AdEdit</Container>;
 }
 
 export default AdEdit;
+
+const Container = styled.main``;
