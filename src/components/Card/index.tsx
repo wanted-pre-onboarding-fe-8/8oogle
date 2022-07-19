9b4e2db181d41fd1271a08184df6509a44ff6187
Aum Il-Kyeong <sunaerocket@gmail.com>
2022-07-19 22:05:05 +0900

    chore: 불필요한 임포트 제거

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent, ReactNode, useState } from 'react';
+import React, { MouseEvent, useState } from 'react';
 import { ICampaignItem } from '../../types/campaign';
 import styled from 'styled-components';
 import { Button, Card as DefaultCard, CardActions, CardContent, CardHeader } from '@mui/material';
