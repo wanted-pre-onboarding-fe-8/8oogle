8a264b7b9fe50af0aad99535b4ac4b543a52e4f8
ChuYeonBin <cndusqls98@gmail.com>
2022-07-19 18:34:43 +0900

    feat: 광고 만들기 버튼 구현

--- src/pages/adManagement/index.tsx
+++ src/pages/adManagement/index.tsx
@@ -2,10 +2,11 @@ import React, { Suspense, useState } from 'react';
 import { CAMPAIGN_CONSTANTS } from '../../utils/constants/data';
 import { typeStatus } from '../../types/campaign';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { Container, FormControl } from '@mui/material';
+import { Button, Container, FormControl } from '@mui/material';
 import MenuItem from '@mui/material/MenuItem';
 import Wrapper from './Wrapper';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 const ALL = '전체';
 const ACTIVE = '진행중';
@@ -15,6 +16,7 @@ type typeStatusKo = typeof ALL | typeof ACTIVE | typeof ENDED;
 
 function AdManagement() {
   const [statusKo, setStatusKo] = useState<typeStatusKo>(ALL);
+  const navigate = useNavigate();
 
   const handleSelectChange = (event: SelectChangeEvent) => {
     const {
@@ -24,17 +26,24 @@ function AdManagement() {
     setStatusKo(value as typeStatusKo);
   };
 
+  const createAd = () => {
+    navigate('/ad/add');
+  };
+
   return (
     <Layout>
-      <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
-        <Select value={statusKo} onChange={handleSelectChange}>
-          {selectItems.map((item: string) => (
-            <MenuItem key={item} value={item}>
-              {item}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+      <ButtonWrapper>
+        <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
+          <Select value={statusKo} onChange={handleSelectChange}>
+            {selectItems.map((item: string) => (
+              <MenuItem key={item} value={item}>
+                {item}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Button onClick={createAd}>광고 만들기</Button>
+      </ButtonWrapper>
 
       <Suspense fallback={<div>Loading</div>}>
         <Wrapper status={getStatusEn(statusKo)} />
@@ -57,7 +66,15 @@ function getStatusEn(statusKo: typeStatusKo): typeStatus {
 }
 
 const Layout = styled(Container)`
+  padding-top: 1rem;
+  padding-bottom: 1rem;
   background-color: white;
   border-radius: 12px;
   overflow-y: scroll;
+`;
+
+const ButtonWrapper = styled.div`
+  margin-bottom: 1.2rem;
+  display: flex;
+  justify-content: space-between;
 `;
