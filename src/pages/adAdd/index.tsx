0f489053b44ab4a6ad0573f2f12ed4cb0536fbfa
bebusl <bebus1998@naver.com>
2022-07-20 15:02:44 +0900

    fix: 중복 id생성 방지, 밸리데이터 수정

--- src/pages/adAdd/index.tsx
+++ src/pages/adAdd/index.tsx
@@ -17,10 +17,14 @@ function AdAdd() {
 
   const validateValues = (values: ICampaignItemBase) => {
     for (const [key, value] of Object.entries(values)) {
-      if (key !== 'endDate' && !value) {
-        if (typeof value === 'number' && value >= 0) {
-          continue;
-        }
+      if (key == 'endDate') continue;
+      if (typeof value === 'number' && value > 0) continue;
+      if (key === 'report') {
+        const assemble = Object.values(values[key]).every((value) => value > 0);
+        if (assemble) continue;
+        else return false;
+      }
+      if (!value) {
         return false;
       }
     }
@@ -38,12 +42,27 @@ function AdAdd() {
     }
   };
 
+  const initialValue = {
+    id: Date.now(),
+    adType: 'web',
+    title: '',
+    budget: 0,
+    status: CAMPAIGN_CONSTANTS.STATUS_ACTIVE,
+    startDate: '',
+    endDate: null,
+    report: {
+      cost: 0,
+      convValue: 0,
+      roas: 0,
+    },
+  };
+
   return (
     <div>
       {!validValue && (
         <Alert severity='warning'>모든 값을 입력해주세요. 숫자는 0을 초과해야 합니다.</Alert>
       )}
-      <AdForm title='광고추가' onSubmit={onSubmit} />
+      <AdForm title='광고추가' campaignItem={initialValue} onSubmit={onSubmit} />
     </div>
   );
 }
