6836b7fb6631e7dc87a656639a6bc3c072c80c20
bebusl <bebus1998@naver.com>
2022-07-20 12:53:30 +0900

    feat: 공란 있을 경우 요청보내지 않고, 안내창 띄우도록 수정

--- src/pages/adAdd/index.tsx
+++ src/pages/adAdd/index.tsx
@@ -9,17 +9,40 @@ import { useQueryClient } from 'react-query';
 
 function AdAdd() {
   const navigate = useNavigate();
-  const { mutateAsync } = createCampaign();
 
+  const { mutateAsync } = createCampaign();
   const queryClient = useQueryClient();
+
+  const [validValue, setValidValue] = useState(true);
+
+  const validateValues = (values: ICampaignItemBase) => {
+    for (const [key, value] of Object.entries(values)) {
+      if (key !== 'endDate' && !value) {
+        if (typeof value === 'number' && value >= 0) {
+          continue;
+        }
+        return false;
+      }
+    }
+    return true;
+  };
+
   const onSubmit = async (values: ICampaignItemBase) => {
-    await mutateAsync(values);
-    await invalidateQueriesByName(queryClient, CAMPAIGN_CONSTANTS.CAMPAIGN);
-    navigate('/ad');
+    if (validateValues(values)) {
+      setValidValue(true);
+      await mutateAsync(values);
+      await invalidateQueriesByName(queryClient, CAMPAIGN_CONSTANTS.CAMPAIGN);
+      navigate('/ad');
+    } else {
+      setValidValue(false);
+    }
   };
 
   return (
     <div>
+      {!validValue && (
+        <Alert severity='warning'>모든 값을 입력해주세요. 숫자는 0을 초과해야 합니다.</Alert>
+      )}
       <AdForm
         title='광고추가'
         onSubmit={onSubmit}
